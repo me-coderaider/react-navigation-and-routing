380ea74e56262f679e654ee7bcc4ca7dad103779
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-23 00:17:19 +0530

    Refactoring with Organziation

--- src/pages/ButtonPage.js
+++ src/pages/ButtonPage.js
@@ -1,6 +1,6 @@
 import { GoBellFill, GoCheckbox } from "react-icons/go";
-import Button from "./components/Button";
-function App() {
+import Button from "../components/Button";
+function ButtonPage() {
     const handleClick = () => {
         console.log("Click !!");
     };
@@ -44,4 +44,4 @@ function App() {
     );
 }
 
-export default App;
+export default ButtonPage;
