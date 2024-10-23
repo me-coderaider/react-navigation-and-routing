2ea303953ec3c8f2ac5eb3be0ba61c14edab4fba
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-23 13:47:33 +0530

    finding EXPANDED item and addition CONDITIONAL RENDERING

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
-import { GoBellFill, GoCheckbox } from "react-icons/go";
-import Button from "./components/Button";
-function App() {
-    const handleClick = () => {
-        console.log("Click !!");
-    };
+import Accordion from "./components/Accordion";
 
-    return (
-        <div>
-            <div>
-                <Button
-                    secondary
-                    rounded
-                    className="mb-5"
-                    onClick={handleClick}
-                >
-                    <GoBellFill />
-                    Click Here!
-                </Button>
-            </div>
-            <div>
-                <Button success outline>
-                    <GoCheckbox />
-                    Buy Now!
-                </Button>
-            </div>
-            <div>
-                <Button warning rounded>
-                    See Deals!
-                </Button>
-            </div>
-            <div>
-                <Button danger rounded outline>
-                    Hide Ads!
-                </Button>
-            </div>
-            <div>
-                <Button primary>Sale!</Button>
-            </div>
-            <div>
-                <Button>Neutral!</Button>
-            </div>
-        </div>
-    );
+function App() {
+    const items = [
+        {
+            id: "adsf",
+            label: "Can I use React on a Project?",
+            content:
+                "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
+        },
+        {
+            id: "adsfasdfa",
+            label: "Can I use Javascript on a Project?",
+            content:
+                "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
+        },
+        {
+            id: "adsffa8sdf",
+            label: "Can I use CSS on a Project?",
+            content:
+                "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
+        },
+    ];
+
+    return <Accordion items={items} />;
 }
 
 export default App;
---
+++ src/components/Accordion.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function Accordion({ items }) {
+    const [expandedIndex, setExpandedIndex] = useState(0);
+
+    const renderedItems = items.map((item, index) => {
+        const isExpanded = index === expandedIndex;
+
+        // const content = isExpanded && <div>{item.content}</div>;
+        // this will as per rule of &&, will give 'first-falsy' or 'last-truthy' value
+
+        return (
+            <div key={item.id}>
+                <div>{item.label}</div>
+                {isExpanded && <div>{item.content}</div>}
+            </div>
+        );
+    });
+
+    return <div>{renderedItems}</div>;
+}
+
+export default Accordion;
