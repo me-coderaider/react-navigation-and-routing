992bd8519b666be967f29fc81f5f056db6d7fa6b
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-23 16:15:08 +0530

    inline event-handler for ACCORDION label

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -11,7 +11,13 @@ function Accordion({ items }) {
 
         return (
             <div key={item.id}>
-                <div>{item.label}</div>
+                <div
+                    onClick={() => {
+                        setExpandedIndex(index);
+                    }}
+                >
+                    {item.label}
+                </div>
                 {isExpanded && <div>{item.content}</div>}
             </div>
         );
