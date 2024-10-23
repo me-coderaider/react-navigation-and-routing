b0f70c63c109302711080b5ba80d2d8621480912
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-23 17:42:38 +0530

    variations of event-handler when element is being created inside MAPPING function

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,21 +3,35 @@ import { useState } from "react";
 function Accordion({ items }) {
     const [expandedIndex, setExpandedIndex] = useState(0);
 
+    // variation 2::
+    const handleClick = (nextIndex) => {
+        setExpandedIndex(nextIndex);
+    };
+
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
 
         // const content = isExpanded && <div>{item.content}</div>;
         // this will as per rule of &&, will give 'first-falsy' or 'last-truthy' value
 
+        // variation 1:: a little variation for the inline event-handler function
+        // const handleClick = () => {
+        //     setExpandedIndex(index);
+        // };
+
         return (
             <div key={item.id}>
-                <div
+                {/* <div
                     onClick={() => {
                         setExpandedIndex(index);
                     }}
                 >
                     {item.label}
-                </div>
+                </div> */}
+
+                {/* variation 1:: <div onClick={handleClick}>{item.label}</div> */}
+                {/* variation 2:: */}
+                <div onClick={() => handleClick(index)}>{item.label}</div>
                 {isExpanded && <div>{item.content}</div>}
             </div>
         );
