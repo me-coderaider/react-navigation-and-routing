98866147fd4a0d28a028a77f131200951cded172
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-23 17:57:02 +0530

    Adding icons and style using TailwindCSS

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { GoChevronDown, GoChevronRight } from "react-icons/go";
 
 function Accordion({ items }) {
     const [expandedIndex, setExpandedIndex] = useState(0);
@@ -10,6 +11,11 @@ function Accordion({ items }) {
 
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
+        const icon = (
+            <span className="text-2xl">
+                {isExpanded ? <GoChevronDown /> : <GoChevronRight />}
+            </span>
+        );
 
         // const content = isExpanded && <div>{item.content}</div>;
         // this will as per rule of &&, will give 'first-falsy' or 'last-truthy' value
@@ -31,13 +37,21 @@ function Accordion({ items }) {
 
                 {/* variation 1:: <div onClick={handleClick}>{item.label}</div> */}
                 {/* variation 2:: */}
-                <div onClick={() => handleClick(index)}>{item.label}</div>
-                {isExpanded && <div>{item.content}</div>}
+                <div
+                    className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
+                    onClick={() => handleClick(index)}
+                >
+                    {item.label}
+                    {icon}
+                </div>
+                {isExpanded && (
+                    <div className="border-b p-5">{item.content}</div>
+                )}
             </div>
         );
     });
 
-    return <div>{renderedItems}</div>;
+    return <div className="border-x border-t rounded">{renderedItems}</div>;
 }
 
 export default Accordion;
