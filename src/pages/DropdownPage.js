5af96f5a680050132d46bcaba8e745b6bd474c34
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-26 22:55:13 +0530

    Separate DropdownPage.js file

---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+import Dropdown from "../components/Dropdown";
+function DropdownPage() {
+    // default 'null' as this is what we came up with during 'State Design Process'
+    const [selection, setSelection] = useState(null);
+
+    const handleSelect = (option) => {
+        setSelection(option);
+    };
+
+    const options = [
+        {
+            label: "Red",
+            value: "red",
+        },
+        {
+            label: "Green",
+            value: "green",
+        },
+        {
+            label: "Blue",
+            value: "blue",
+        },
+    ];
+
+    return (
+        <div className="flex">
+            <Dropdown
+                options={options}
+                value={selection}
+                onChange={handleSelect}
+            />
+        </div>
+    );
+}
+
+export default DropdownPage;
