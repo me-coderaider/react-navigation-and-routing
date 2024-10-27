7d5108c294a4fa2e4c1293e52e00e58ad8d99d54
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-27 17:13:54 +0530

    dealing with ctrl+click navigation and Link styling

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,17 +1,30 @@
+import classNames from "classnames";
 import { useContext } from "react";
 import NavigationContext from "../context/navigation";
 
 function Link({ to, children }) {
     const { navigate } = useContext(NavigationContext);
 
+    const classes = classNames("text-blue-500");
+
     const handleClick = (event) => {
+        // console.log(event);
+        // for 'ctrl' button + click has done or not.
+        if (event.metaKey || event.ctrlKey) {
+            return;
+        }
+
         // STOPPING THE PAGE REFRESH
         event.preventDefault();
 
         navigate(to);
     };
 
-    return <a onClick={handleClick}>{children}</a>;
+    return (
+        <a className={classes} href={to} onClick={handleClick}>
+            {children}
+        </a>
+    );
 }
 
 export default Link;
