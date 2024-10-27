1253a55e30e40c5e5cc7c626aef1fffe48ad1e67
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-27 18:04:09 +0530

    hightlighting ACTIVE link of the Sidebar component

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,11 +4,15 @@ import classNames from "classnames";
 
 import useNavigation from "../hooks/use-navigation";
 
-function Link({ to, children }) {
+function Link({ to, children, className, activeClassName }) {
     // const { navigate } = useContext(NavigationContext);
-    const { navigate } = useNavigation();
+    const { navigate, currentPath } = useNavigation();
 
-    const classes = classNames("text-blue-500");
+    const classes = classNames(
+        "text-blue-500",
+        className,
+        currentPath === to && activeClassName
+    );
 
     const handleClick = (event) => {
         // console.log(event);
