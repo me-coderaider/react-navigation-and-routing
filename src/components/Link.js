c7273c257835e2b10c286288bfe8c18ba46297c5
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-27 17:20:47 +0530

    adding a custom hook i.e. useNavigation()

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,12 @@
 import classNames from "classnames";
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+// import { useContext } from "react";
+// import NavigationContext from "../context/navigation";
+
+import useNavigation from "../hooks/use-navigation";
 
 function Link({ to, children }) {
-    const { navigate } = useContext(NavigationContext);
+    // const { navigate } = useContext(NavigationContext);
+    const { navigate } = useNavigation();
 
     const classes = classNames("text-blue-500");
 
