d9fc39acdc3386ffd6159ea9028710b82dab0f8f
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-28 17:25:16 +0530

    Closing the 'Modal' component

--- src/pages/ModalPage.js
+++ src/pages/ModalPage.js
@@ -10,13 +10,17 @@ function ModalPage() {
         setShowModal(true);
     };
 
+    const handleClose = () => {
+        setShowModal(false);
+    };
+
     return (
         // <div className="relative"> it won't break the MODAL now!!
         <div>
             <Button onClick={handleClick} primary>
                 Open Modal
             </Button>
-            {showModal && <Modal />}
+            {showModal && <Modal onClose={handleClose} />}
         </div>
     );
 }
