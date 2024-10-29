ff952b59e0830d9f646a6dbbf9d8bdf0e1e3500e
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-29 17:13:08 +0530

    passing 'key' using a 'keyFn' function from parent

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,20 +1,20 @@
-function Table({ data, config }) {
+function Table({ data, config, keyFn }) {
     const renderedHeaders = config.map((column) => {
         return <th key={column.label}>{column.label}</th>;
     });
 
-    const renderedRows = data.map((fruit) => {
+    const renderedRows = data.map((rowData) => {
         const renderedCells = config.map((column) => {
             return (
                 <td className="p-3" key={column.label}>
-                    {column.render(fruit)}
+                    {column.render(rowData)}
                 </td>
             );
         });
 
         // 3rd way -- Dynamic generation of cells of each row.
         return (
-            <tr className="border-b" key={fruit.name}>
+            <tr className="border-b" key={keyFn(rowData)}>
                 {renderedCells}
             </tr>
         );
