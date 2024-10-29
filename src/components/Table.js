123bd2a40194d992926e9c7277dda459d88cf257
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-29 17:03:12 +0530

    DYNAMIC generation of cells for each ROW of the table using NESTED-MAPS

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,18 +4,33 @@ function Table({ data, config }) {
     });
 
     const renderedRows = data.map((fruit) => {
+        const renderedCells = config.map((column) => {
+            return (
+                <td className="p-3" key={column.label}>
+                    {column.render(fruit)}
+                </td>
+            );
+        });
+
+        // 3rd way -- Dynamic generation of cells of each row.
         return (
             <tr className="border-b" key={fruit.name}>
-                {/* <td className="p-3">{fruit.name}</td> */}
-                <td className="p-3">{config[0].render(fruit)}</td>
-
-                {/* <td className="p-3"><div className={`p-3 m-2 ${fruit.color}`}></div></td> */}
-                <td className="p-3">{config[1].render(fruit)}</td>
-
-                {/* <td className="p-3">{fruit.score}</td> */}
-                <td className="p-3">{config[2].render(fruit)}</td>
+                {renderedCells}
             </tr>
         );
+
+        // return (
+        //     <tr className="border-b" key={fruit.name}>
+        //         {/* 1st way <td className="p-3">{fruit.name}</td> */}
+        //         <td className="p-3">{config[0].render(fruit)}</td>
+
+        //         {/* 1st way <td className="p-3"><div className={`p-3 m-2 ${fruit.color}`}></div></td> */}
+        //         <td className="p-3">{config[1].render(fruit)}</td>
+
+        //         {/* 1st way <td className="p-3">{fruit.score}</td> */}
+        //         <td className="p-3">{config[2].render(fruit)}</td>
+        //     </tr>
+        // );
     });
 
     return (
