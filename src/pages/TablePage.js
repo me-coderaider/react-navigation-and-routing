fc808678e6f9d77e05642fd0a498ab47220d98f4
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-29 14:38:19 +0530

    populating 'table' with data and using 'thead' & 'tbody' elements

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,9 +1,32 @@
 import Table from "../components/Table";
 
 function TablePage() {
+    const data = [
+        {
+            name: "Orange",
+            color: "bg-orange-500",
+            score: 5,
+        },
+        {
+            name: "Apple",
+            color: "bg-red-500",
+            score: 5,
+        },
+        {
+            name: "Banana",
+            color: "bg-yellow-500",
+            score: 5,
+        },
+        {
+            name: "Lime",
+            color: "bg-green-500",
+            score: 5,
+        },
+    ];
+
     return (
         <div>
-            <Table></Table>
+            <Table data={data} />
         </div>
     );
 }
