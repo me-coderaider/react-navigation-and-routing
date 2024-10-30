225fa35d7309dbc1bfdde7824f69117d0f33b1d2
me-coderaider <kashyappankajnsp@gmail.com>
2024-10-31 00:26:25 +0530

    creating a separate 'hook' file and using in 'CounterPage' file

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,23 +1,5 @@
-import { useState, useEffect } from "react";
 import Button from "../components/Button";
-
-// function useSomething(initialCount) {
-function useCounter(initialCount) {
-    const [count, setCount] = useState(initialCount);
-
-    useEffect(() => {
-        console.log(count);
-    }, [count]);
-
-    const increment = () => {
-        setCount(count + 1);
-    };
-
-    return {
-        count: count,
-        increment,
-    };
-}
+import useCounter from "../hooks/use-counter";
 
 function CounterPage({ initialCount }) {
     const { count, increment } = useCounter(initialCount);
